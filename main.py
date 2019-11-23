e5c31b5e4f9adc8eef25f7328fea348c9052a9cf
Andrew Attard <andrew.attard@outlook.com>
2019-11-23 18:27:23 +0700

    Sample to test merge from Truman

--- main.py
+++ main.py
@@ -2,6 +2,36 @@
 # main.py - Primary script for project Oscar, a tool for identifying and grouping
 # 			similar images locally
 
-# imports
+# Import the required modules
+from PIL import Image
+import imagehash
+import argparse
+import shelve
+import glob
 
-#
+# Create the argument parser object
+ap = argparse.ArgumentParser(description='Parse source directory and shelf arguments')
+ap.add_argument("-d", "--dataset", required = True,
+    help = "path to input dataset of images")
+ap.add_argument("-s", "--shelve", required = True,
+    help = "output shelve database")
+args = vars(ap.parse_args())
+print(args)
+
+# open the shelve database
+db = shelve.open(args["shelve"], writeback = True)
+
+# loop over the image dataset
+for imagePath in glob.glob(args["dataset"] + "/*.jpg"):
+    # load the image and compute the difference hash
+    image = Image.open(imagePath)
+    h = str(imagehash.dhash(image))
+
+    # extract the filename from the path and update the database
+    # using the hash as the key and the filename append to the
+    # list of values
+    filename = imagePath[imagePath.rfind("/") + 1:]
+    db[h] = db.get(h, []) + [filename]
+
+# close the shelf database
+db.close()
