ecbdad3f29dd1d8804998f56fd16bb1cc1d91dca
Truman Leung <truman.leung@gmail.com>
2019-12-08 09:50:57 -0500

    tried to add unit test for arguments issue #12

---
+++ unit_test.py
@@ -0,0 +1,11 @@
+import main
+
+def test_increment():
+    assert main.parse_args(3) == 4
+
+'''
+def firstTest(self):
+    args = main.parse_args('-d a -s b')
+    self.assertTrue.isinstance(args, list)
+
+'''
